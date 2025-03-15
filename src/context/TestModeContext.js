d69ad9c7d194bb5ffad0445f0ae61adc395da19c
shubham3246 <raj2013atel@gmail.com>
2025-03-15 22:31:52 +0530

    formatted and cleaned some code

--- src/context/TestModeContext.js
+++ src/context/TestModeContext.js
@@ -2,17 +2,15 @@ import { createContext, useContext, useState } from "react";
 
 const TestModeContext = createContext();
 
-export const TestModeContextProvider= ({children})=>{
-  
-    
-    const [testTime,setTestTime] = useState(15);
+export const TestModeContextProvider = ({ children }) => {
+    const [testTime, setTestTime] = useState(15);
 
-    const values={
+    const values = {
         testTime,
         setTestTime
     }
 
-
     return (<TestModeContext.Provider value={values}>{children}</TestModeContext.Provider>)
 }
-    export const useTestMode =()=>useContext(TestModeContext);
+
+export const useTestMode = () => useContext(TestModeContext);
